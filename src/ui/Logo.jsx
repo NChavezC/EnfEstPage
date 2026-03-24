677e7d2571764a8cf6fe0d0cfef479e40502b088
NChavezC <gchavez@fen.uchile.cl>
2026-03-24 16:49:23 -0300

    fix logo

--- src/ui/Logo.jsx
+++ src/ui/Logo.jsx
@@ -1,7 +1,7 @@
 function Logo() {
   return (
-    <div className="flex items-center">
-      <img src="logo-new.jpeg" alt="Logo" className="h-20 w-auto" />
+    <div className="flex items-center shrink-0">
+      <img src="logo-new.jpeg" alt="Logo" className="h-20 w-auto shrink-0" />
     </div>
   );
 }
